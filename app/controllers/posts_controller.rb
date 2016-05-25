df25a531d48fec1e4102c55a7e56db0b82dbc3fb
CSKingMartin <cwells@linfield.edu>
2016-05-25 09:58:53 -0700

    Morning commits as of 5/25

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,31 @@
 class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
 
 	def show
 		@post = Post.find(params[:id])
 	end
 
 	def new
-
+		@post = Post.new
 	end
+
+	def edit
+		@post = Post.find(params[:id])
 
 	def create
 		@post = Post.new(post_params)
 		
 
-		@post.save
+		if @post.save 
+			redirect_to @post
+		else
+			render 'new'
+		end
 
-		redirect_to @post
+		
 	end
 
 	private
